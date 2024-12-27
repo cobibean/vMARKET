387ab689aa9f9e8cb3c1d73c202521d6b2f36180
cobibean <cobibean777@gmail.com>
2024-12-27 13:46:09 -0800

    help

--- src/app/client.ts
+++ src/app/client.ts
@@ -1,6 +1,13 @@
-// src/client.ts
 import { createThirdwebClient } from "thirdweb";
 
+// Replace this with your client ID string
+// refer to https://portal.thirdweb.com/typescript/v5/client on how to get a client ID
+const clientId = "d4dd39d0c84df782e1bbdfef93b744b4";
+
+if (!clientId) {
+  throw new Error("No client ID provided");
+}
+
 export const client = createThirdwebClient({
-  clientId: process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID as string,
+  clientId: clientId,
 });

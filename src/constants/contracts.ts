2b001af8bf3f9f41522bdc865470752145e532dd
cobibean <cobibean777@gmail.com>
2024-12-30 13:13:18 -0800

    contractupdate to add [] refund

--- src/constants/contracts.ts
+++ src/constants/contracts.ts
@@ -2,7 +2,7 @@ import { client } from "@/app/client";
 import { getContract } from "thirdweb";
 import { defineChain } from "thirdweb"; // Ensure this import is correct based on your project structure
 
-export const predictionMarketContractAddress = "0xE0DC88B9403ee5311b98117531993d0Ac6Da6c77";
+export const predictionMarketContractAddress = "0xC4043B7C1ab715be331bEBa9f186E99912830eeb";
 export const tokenContractAddress = "0x848E329d9C3FF5D3078C4670c773651155386C46"; 
 
 export const contract = getContract({

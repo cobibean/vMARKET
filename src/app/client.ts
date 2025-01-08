029d07bd896498b095d44915acde14fd62027385
cobibean <cobibean777@gmail.com>
2025-01-08 16:05:19 -0600

    New Contract Address

--- src/app/client.ts
+++ src/app/client.ts
@@ -1,7 +1,5 @@
 import { createThirdwebClient } from "thirdweb";
 
-// Replace this with your client ID string
-// refer to https://portal.thirdweb.com/typescript/v5/client on how to get a client ID
 const clientId = "d4dd39d0c84df782e1bbdfef93b744b4";
 
 if (!clientId) {
--- src/constants/contracts.ts
+++ src/constants/contracts.ts
@@ -1,8 +1,8 @@
 import { client } from "@/app/client";
 import { getContract } from "thirdweb";
-import { defineChain } from "thirdweb"; // Ensure this import is correct based on your project structure
+import { defineChain } from "thirdweb"; 
 
-export const predictionMarketContractAddress = "0x32ce242630c39A94EC24742025d2BE4D380DB8b5";
+export const predictionMarketContractAddress = "0xf1f18D31841DdFBB254BA1F487619d7c9A9fA41e";
 export const tokenContractAddress = "0x848E329d9C3FF5D3078C4670c773651155386C46"; 
 
 export const contract = getContract({
